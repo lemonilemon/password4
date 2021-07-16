2764577b815986933561f97ba35d4e7c274b4b3e
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-07-17 00:38:45 +0800

    Update 密碼.js

--- 密碼.js
+++ 密碼.js
@@ -2,7 +2,7 @@ function send(rep1) {
     let ans1 = "1010101010";
     let ans2 = "monkey";
     let ans3 = "orz";
-    if(ans == rep1) {
+    if(ans1 == rep1) {
         alert("好像有了甚麼動靜\n下面那是傳送門嗎？");
         document.write("<a href = './router/router.html'><div>我是傳送門</div></a>");
     }
