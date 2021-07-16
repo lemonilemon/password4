98922d6be15834b00b7e8bcecf5836522c3e39fd
lemonilemon <jaspertsai1019@gmail.com>
2021-07-17 00:49:46 +0800

    finally

--- 密碼.js
+++ 密碼.js
@@ -1,8 +1,8 @@
-function send(rep1) {
+function send(rep1, rep2, rep3) {
     let ans1 = "1010101010";
     let ans2 = "monkey";
     let ans3 = "orz";
-    if(ans1 == rep1) {
+    if(ans1 == rep1 && ans2 == rep2 && ans3 == rep3) {
         alert("好像有了甚麼動靜\n下面那是傳送門嗎？");
         document.write("<a href = './router/router.html'><div>我是傳送門</div></a>");
     }
