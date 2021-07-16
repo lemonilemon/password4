76e23c561d743e4f06994091ea00d4cfffd233d2
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-07-17 00:22:21 +0800

    Initial commit

---
+++ 密碼.js
@@ -0,0 +1,10 @@
+function send(rep) {
+    let ans = "BreakALLsodianorz";
+    if(ans == rep) {
+        alert("好像有了甚麼動靜\n下面那是傳送門嗎？");
+        document.write("<a href = './router/router.html'><div>我是傳送門</div></a>");
+    }
+    else {
+        alert("密碼並不正確，再想想吧");
+    }
+}
